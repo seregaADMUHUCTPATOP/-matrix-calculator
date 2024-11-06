8ae5ebfd6d2eff46ade6038a7a2d13746f57bf09
evgen-palych <onikevicz@gmail.com>
2024-11-07 01:07:02 +0800

    Add files via upload
    Signed-off-by: evgen-palych <onikevicz@gmail.com>

---
+++ Source1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+vector<vector<int>> inputMatrix(int rows, int cols) {
+    setlocale(LC_ALL, "Russian");
+    vector<vector<int>> matrix(rows, vector<int>(cols));
+    cout << "¬ведите элементы матрицы:" << endl;
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cin >> matrix[i][j];
+        }
+    }
+    return matrix;
+}
+void printMatrix(const vector<vector<int>>& matrix) {
+    for (const auto& row : matrix) {
+        for (int element : row) {
+            cout << element << " ";
+        }
+        cout << endl;
+    }
+}
